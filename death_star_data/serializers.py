6ff757bce2f4759f71cf243b2183182cfde985eb
John Wood <johnnathanwood@gmail.com>
2019-02-21 11:03:20 -0600

    added serializer file and set up views file

---
+++ death_star_data/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers
+from death_star_data.models import Customer, Product, ProductType, PaymentType, Order, ProductOrder, Department, Employee, Training, EmployeeTraining, Computer, ComputerEmployee
+
