035ef247c7cf60eb3c37e3c0b9ec084f594c4136
buffard <samuelwebber19@gmail.com>
2019-02-21 16:11:22 -0600

    able to filter training programs

--- death_star_data/views.py
+++ death_star_data/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.decorators import action
-from rest_framework import filters
 from death_star_data.models import Customer, Product, ProductType, PaymentType, Order, ProductOrder, Department, Employee, Training, EmployeeTraining, Computer, ComputerEmployee
 from death_star_data.serializers import TrainingSerializers
+import datetime
 
 
 
@@ -14,15 +14,30 @@ def api_root(request, format=None):
          'training': reverse('training', request=request, format=format),
     })
 
+
 class TrainingViewSet(viewsets.ModelViewSet):
     queryset = Training.objects.all()
     serializer_class = TrainingSerializers
+    
 
-    def delete(self, pk, request):
-        password = request.data.get('password')
-        article_id = request.data.get('article')
-        article_instance = article.objects.get(id=article_id)
-        if password == article_instance.password:
-            article_instance.delete()
-            return Response(status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_304_NOT_MODIFIED)
+    def get_queryset(self):
+        current_date = datetime.date.today()
+        query_set = Training.objects.all()
+        keyword = self.request.query_params.get('completed')
+        if keyword == 'true':    
+            print("query params", keyword)
+            print("HERE", current_date)
+            query_set = query_set.filter(end_date__lt=current_date)
+        elif keyword == 'false':
+            query_set = query_set.filter(end_date__gte=current_date)
+        return query_set
+    
+
+    
+    
+
+
+  
+
+    
+        
