a76257d72ba669fadc3e7aa5be4472838d81584b
John Wood <johnnathanwood@gmail.com>
2019-02-21 11:05:54 -0600

    fixed views

--- death_star_data/views.py
+++ death_star_data/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+
+from rest_framework import filters
+
+from death_star_data.models import Customer, Product, ProductType, PaymentType, Order, ProductOrder, Department, Employee, Training, EmployeeTraining, Computer, ComputerEmployee
+from death_star_data.serializers import 
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+         
+    })
